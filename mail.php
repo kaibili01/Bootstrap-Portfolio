0bbcefd3f9ecb3d6186b710acb487b375f2b7c5c
Adan <adananimaldesign@gmail.com>
2019-07-23 12:17:03 -0700

    modified PHP to outlook instead of gmail

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
 
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "adananimaldesign@gmail.com";
+$recipient = "lumbre49@hotmail.com.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
