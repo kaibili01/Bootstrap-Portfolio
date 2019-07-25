b81a53104a8d37503d9fc570ce87de3241144f04
Adan <adananimaldesign@gmail.com>
2019-07-25 10:16:55 -0700

    new contact page

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
-if(isset( $_POST['contact-form']))
+if(isset( $_POST['name']))
 $name = $_POST['name'];
-if(isset( $_POST['contact-form']))
+if(isset( $_POST['email']))
 $email = $_POST['email'];
-if(isset( $_POST['contact-form']))
+if(isset( $_POST['message']))
 $message = $_POST['message'];
-if(isset( $_POST['contact-form']))
+if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
 
 $content="From: $name \n Email: $email \n Message: $message";
